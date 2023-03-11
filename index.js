afb2b3bf4dfd55e35590c6cf5e2731e0ff21ac65
kaf-lamed-beyt <belac335@gmail.com>
2023-03-11 18:59:01 +0100

    feat: saveToLocalStorage()

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 const INSERT_TWEET = document.querySelector(".app-form");
 const SAVE_BUTTON = document.querySelector(".save-tweet");
-const TWEET_TITLE = document.querySelector("#title");
-const TWEET_LINK = document.querySelector("#link");
+const TITLE_FEILD = document.querySelector("#title");
+const LINK_FEILD = document.querySelector("#link");
 
-// validate tweet url
-function validateTweetUrl() {
-  if (!TWEET_LINK.value.includes("https://twitter.com")) {
-    console.log("invalid Tweet link");
+// modals
+const ERROR_MODAL = document.querySelector(".error");
+const SUCCESS_MODAL = document.querySelector(".success");
+
+// validate input fields
+function validateInputs() {
+  const tweetTitle = document.querySelector("#title").value;
+  const tweetLink = document.querySelector("#link").value;
+
+  if (!tweetLink.includes("https://twitter.com")) {
+    ERROR_MODAL.style.display = "block";
+    ERROR_MODAL.innerHTML = "invalid Tweet link";
+    LINK_FEILD.focus();
+  } else if (!tweetTitle) {
+    ERROR_MODAL.innerHTML =
+      "I know you have a retentive memory, but won't it be nice to search for these links with their titles?";
+    ERROR_MODAL.style.display = "block";
+    TITLE_FEILD.focus();
+  } else if (!tweetLink) {
+    ERROR_MODAL.innerHTML = "I think you forgot to add a tweet link";
+    ERROR_MODAL.style.display = "block";
+    LINK_FEILD.focus();
+  } else if (!tweetLink && !tweetTitle) {
+    ERROR_MODAL.innerHTML =
+      "Don't anger the gods! Please enter a tweet link and the title.";
+    ERROR_MODAL.style.display = "block";
   } else {
-    console.log("This link is valid");
+    ERROR_MODAL.style.display = "none";
   }
+}
+
+function saveToLocalStorage() {
+  const title = document.querySelector("#title").value;
+  const link = document.querySelector("#link").value;
+
+  let tweets = [JSON.parse(localStorage.getItem("tweets"))];
+
+  console.log(tweets);
+
+  tweets.push({ title, link });
+
+  localStorage.setItem("tweets", JSON.stringify(tweets));
 }
 
 INSERT_TWEET.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  validateTweetUrl();
+  validateInputs();
+  saveToLocalStorage();
 });
