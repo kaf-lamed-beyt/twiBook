51927cb4098f4d8b7eceeb7b70e65d091da3b119
kaf-lamed-beyt <belac335@gmail.com>
2024-01-31 16:17:09 +0100

    chore: base setup

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { Center, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Center height="100vh">
+      <Text as="h1" textAlign="center" fontSize="80px" fontWeight="900">
+        Hello twiBook v2.0
+      </Text>
+    </Center>
+  );
+}
+
+export default App;
