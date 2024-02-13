357cc7dc8a56f8b5065d6a6d1054d879074ed525
kaf-lamed-beyt <belac335@gmail.com>
2024-02-13 14:31:35 +0100

    minor

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,27 +1,27 @@
-import {
-  pgTable,
-  text,
-  serial,
-  boolean,
-  date,
-  uuid,
-  jsonb,
-} from "drizzle-orm/pg-core";
+// import {
+//   pgTable,
+//   text,
+//   serial,
+//   boolean,
+//   date,
+//   uuid,
+//   jsonb,
+// } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  email: text("email"),
-  fullName: text("full_name"),
-  hasLicense: boolean("Pro"),
-  userId: uuid("user_id"),
-  books: jsonb("books").default([]),
-});
+// export const users = pgTable("users", {
+//   id: serial("id").primaryKey(),
+//   email: text("email"),
+//   fullName: text("full_name"),
+//   hasLicense: boolean("Pro"),
+//   userId: uuid("user_id"),
+//   books: jsonb("books").default([]),
+// });
 
-export const books = pgTable("books", {
-  id: serial("id").primaryKey(),
-  title: text("title"),
-  content: text("content"),
-  author: text("author"),
-  date: date("date"),
-  bookId: uuid("book_id"),
-});
+// export const books = pgTable("books", {
+//   id: serial("id").primaryKey(),
+//   title: text("title"),
+//   content: text("content"),
+//   author: text("author"),
+//   date: date("date"),
+//   bookId: uuid("book_id"),
+// });
