9d8920376be7149866347b77352c0dd49b2663a7
kaf-lamed-beyt <belac335@gmail.com>
2024-03-08 12:09:04 +0100

    chore: add a proxy setup with redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/tweet",
+        destination: "https://api.twitter.com",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- pages/api/tweet.ts
+++ pages/api/tweet.ts
@@ -2,16 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Rettiwt } from "rettiwt-api";
 
 type TweetId = string | string[] | undefined;
-const retweetInstance = new Rettiwt({
-  proxyUrl: new URL(process.env.NEXT_PUBLIC_PROXY_URL as string),
-});
+// const retweetInstance = new Rettiwt({
+//   proxyUrl: new URL(`${process.env.NEXT_PUBLIC_PROXY_URL}`, "/api/tweet"),
+// });
+
+const retweetInstance = new Rettiwt();
 
 export default async function tweetApiRoute(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const tweetId: TweetId = req.query.tweetId;
-
   const response = await retweetInstance.tweet.details(tweetId as string);
 
   res.send(response);
