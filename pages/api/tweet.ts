128ea51d3f5371b600d04507ac7367088fe8765c
kaf-lamed-beyt <belac335@gmail.com>
2024-03-08 01:52:33 +0100

    update

--- pages/api/tweet.ts
+++ pages/api/tweet.ts
@@ -2,13 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Rettiwt } from "rettiwt-api";
 
 type TweetId = string | string[] | undefined;
+const retweetInstance = new Rettiwt();
 
 export default async function tweetApiRoute(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const tweetId: TweetId = req.query.tweetId;
-  const retweetInstance = new Rettiwt();
 
   const response = await retweetInstance.tweet.details(tweetId as string);
 
