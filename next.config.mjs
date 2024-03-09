de66915787f694faf0ecd655ba91c99d0adb125a
kaf-lamed-beyt <belac335@gmail.com>
2024-03-09 09:47:57 +0100

    chore: setup proxying in next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/tweet",
-  //       destination: "https://api.twitter.com",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/tweet",
+        destination: "https://api.twitter.com/1.1",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
